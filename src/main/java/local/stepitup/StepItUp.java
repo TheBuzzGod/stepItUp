89608b9ca8aed2348d2a3163ef10206babde8d6a
BuildTools <unconfigured@null.spigotmc.org>
2026-08-12 01:55:41 -0400

    refactored mod info.

--- src/main/java/local/stepitup/StepItUp.java
+++ src/main/java/local/stepitup/StepItUp.java
@@ -1,4 +1,4 @@
-package turniplabs.examplemod;
+package local.stepitup;
 
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
@@ -7,15 +7,15 @@ import turniplabs.halplibe.HalpLibe;
 import turniplabs.halplibe.event.defs.CommonEvents;
 import turniplabs.halplibe.util.dependency.Key;
 
-public class ExampleMod implements ModInitializer {
-	public static final String MOD_ID = HalpLibe.registerMod("examplemod", true);
+public class StepItUp implements ModInitializer {
+	public static final String MOD_ID = HalpLibe.registerMod("stepitup", true);
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
 		CommonEvents.BEFORE_GAME_START.listen(Key.of(MOD_ID), this::beforeGameStart);
 		CommonEvents.AFTER_GAME_START.listen(Key.of(MOD_ID), this::afterGameStart);
-		LOGGER.info("ExampleMod initialized.");
+		LOGGER.info("StepItUp initialized.");
 	}
 
 	public void beforeGameStart() {
